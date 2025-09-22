29070408e00b346c46b0942200cbac0dca00e2f7
avjj00 <gobarnacht@protonmail.com>
2025-09-22 11:36:16 +0300

    Update Uninstall-MSI-GUID.ps1

--- Uninstall-MSI-GUID.ps1
+++ Uninstall-MSI-GUID.ps1
@@ -1,4 +1,5 @@
 $uninstallPathList = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
 
-$uninstallString = Get-ChildItem -Path $uninstallPathList | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object { $_.DisplayName -like "*Intune*" } | Select-Object -ExpandProperty UninstallString
+$uninstallString = Get-ChildItem -Path $uninstallPathList | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object { $_.DisplayName -like "*APPLICATION_NAME_HERE*" } | Select-Object -ExpandProperty UninstallString
+
 $uninstallGuid = $uninstallString | Select-String -Pattern '{[-0-9A-F]+?}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
