9de669927604f305cc11dfa88751e5d2fe3a0ff1
avjj00 <gobarnacht@protonmail.com>
2025-10-01 09:43:40 +0300

    Update Uninstall-MSI-GUID.ps1

--- Uninstall-MSI-GUID.ps1
+++ Uninstall-MSI-GUID.ps1
@@ -1,5 +1,17 @@
 $uninstallPathList = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall','HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
 
 $uninstallString = Get-ChildItem -Path $uninstallPathList | ForEach-Object { Get-ItemProperty $_.PSPath } | Where-Object { $_.DisplayName -like "*APPLICATION_NAME_HERE*" } | Select-Object -ExpandProperty UninstallString
-
 $uninstallGuid = $uninstallString | Select-String -Pattern '{[-0-9A-F]+?}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value
+
+try {
+	
+	Start-Process 'msiexec.exe' -ArgumentList "/x $uninstallGuid /norestart /quiet" -WindowStyle Hidden
+	
+} catch {
+					
+	Write-Host "Uninstallation failed."
+	Write-Host "Last error message: $Error[0].Exception.Message"
+					
+	exit 1
+					
+}
